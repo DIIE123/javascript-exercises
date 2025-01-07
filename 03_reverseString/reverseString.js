7758cca80a1043e6de12d2d2e0b4afb921e5e110
DIIE123 <diiew1234@gmail.com>
2025-01-06 21:02:24 -0500

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = "";
 
+    for (let i = 0; i < str.length; i++) {
+        newStr = str.at(i) + newStr;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
