d4edd16c4458677f1623d00716cb5cd8c67e3424
DIIE123 <diiew1234@gmail.com>
2025-01-06 21:19:53 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (arr[i] === arguments[j]) {
+                arr.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
