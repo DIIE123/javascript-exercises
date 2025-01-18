77cdb7543dc5570c6a75965f60c1678949f865b2
DIIE123 <diiew1234@gmail.com>
2025-01-18 15:18:09 -0500

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    if (str === "") return true;
 
+    // Convert string to lowercase and filter out non alphanumeric characters
+    str = str.toLowerCase().split("").filter(isAlphaNumeric).join("");
+
+    return str === str.split("").reverse().join("");
 };
+
+function isAlphaNumeric(str) {
+    let code = str.charCodeAt(0);
+    return (code >= 48 && code <= 57) || (code >= 97 && code <= 122);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
