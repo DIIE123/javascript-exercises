f644b3c52cd6d86a4b6f6f9552aa6a6fa9ad09e5
DIIE123 <diiew1234@gmail.com>
2025-01-18 14:37:03 -0500

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
 
+    let f1 = 0;
+    let f2 = 1;
+
+    for (let i = 0; i < num; i++) {
+        let temp = f1;
+        f1 = f2;
+        f2 += temp;
+    }
+
+    return f1;
 };
 
 // Do not edit below this line
