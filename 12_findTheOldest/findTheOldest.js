06dfac20447175270ca079ac187a6228c5b445e1
DIIE123 <diiew1234@gmail.com>
2025-01-18 15:00:20 -0500

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+let date = new Date();
 
+const findTheOldest = function(arr) {
+    return arr.reduce(compareDates, { name: "", yearOfBirth: date.getFullYear()});
 };
+
+function compareDates(curOldest, person) {
+    let age = 0;
+    
+    if ("yearOfDeath" in person) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    }
+    else {
+        age = date.getFullYear() - person.yearOfBirth;
+    }
+
+    let oldestAge = 0;
+    if ("yearOfDeath" in curOldest) {
+        oldestAge = curOldest.yearOfDeath - curOldest.yearOfBirth;
+    }
+    else {
+        oldestAge = date.getFullYear() - curOldest.yearOfBirth;
+    }
+
+    if (age > oldestAge) return person;
+    else return curOldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
